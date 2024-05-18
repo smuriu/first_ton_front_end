4f2459f8380c043bc35ad12d0e23bb4b4e41c50a
Njenga Muriu <smuriu@gmail.com>
2024-05-18 17:31:01 +0300

    adds telegram web app sdk

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import { useMainContract } from './hooks/useMainContract'
 import { useTonConnect } from './hooks/useTonConnect'
 import { fromNano } from '@ton/core'
+import WebApp from '@twa-dev/sdk'
+import { MainButton } from '@twa-dev/sdk/react'
 
 function App() {
   const {
@@ -16,44 +18,55 @@ function App() {
   } = useMainContract()
   const { connected } = useTonConnect()
 
+  const showAlert = () => {
+    WebApp.showAlert('Hey there!')
+  }
+
   return (
-    <div>
+    <>
       <div>
-        <TonConnectButton />
-      </div>
-      <div>
-        <div className='Card'>
-          <b>Our contract Address</b>
-          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
-          <b>Our contract Balance</b>
-          <div className='Hint'>{fromNano(contract_balance)} TON</div>
+        <div>
+          <TonConnectButton />
         </div>
+        <div>
+          <div className='Card'>
+            <b>{WebApp.platform}</b>
+          </div>
 
-        <div className='Card'>
-          <b>Recent Sender</b>
-          <div>{recent_sender?.toString() ?? 'Loading...'}</div>
-          <b>Counter Value</b>
-          <div>{counter_value ?? "Loading..."}</div>
+          <div className='Card'>
+            <b>Our contract Address</b>
+            <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+            <b>Our contract Balance</b>
+            <div className='Hint'>{fromNano(contract_balance)} TON</div>
+          </div>
+
+          <div className='Card'>
+            <b>Recent Sender</b>
+            <div>{recent_sender?.toString() ?? 'Loading...'}</div>
+            <b>Counter Value</b>
+            <div>{counter_value ?? "Loading..."}</div>
+          </div>
+
+          {connected && (
+            <>
+              <div className='Card'>
+                <button onClick={() => sendIncrement()}>Increase counter by 3</button>
+              </div>
+              <br />
+              <div className='Card'>
+                <button onClick={() => sendDeposit()}>Deposit 1 TON</button>
+              </div>
+              <br />
+              <div className='Card'>
+                <button onClick={() => sendWithdrawalRequest()}>Withdraw 1 TON</button>
+              </div>
+            </>
+          )}
         </div>
-
-        {connected && (
-          <>
-            <div className='Card'>
-              <button onClick={() => sendIncrement()}>Increase counter by 3</button>
-            </div>
-            <br />
-            <div className='Card'>
-              <button onClick={() => sendDeposit()}>Deposit 1 TON</button>
-            </div>
-            <br />
-            <div className='Card'>
-              <button onClick={() => sendWithdrawalRequest()}>Withdraw 1 TON</button>
-            </div>
-          </>
-        )}
+        <a target="_blank" href="https://icons8.com/icon/48332/puzzle">Puzzle</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
       </div>
-      <a target="_blank" href="https://icons8.com/icon/48332/puzzle">Puzzle</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-    </div>
+      <MainButton text="Submit" onClick={() => showAlert()} />
+    </>
   )
 }
 
